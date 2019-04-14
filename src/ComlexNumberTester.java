50abe6c5138b15ae0e2d4a2b70821d86df0fcb78
Sergey Deshunin <deshunin@gmail.com>
2019-04-14 19:15:42 +0300

    I added a div method

--- src/ComlexNumberTester.java
+++ src/ComlexNumberTester.java
@@ -6,12 +6,17 @@ public class ComlexNumberTester {
 		ComplexNumber c1 = new ComplexNumber(1,2);
 		ComplexNumber c2 = new ComplexNumber(3,4);
 		ComplexNumber z = new ComplexNumber(0,0);
+		
 		z = z.add(c1, c2);
 		System.out.println("Let c1 = " + c1.getA() + " + " + c1.getB()+ "i" + " and c2 = " + c2.getA() + " + " + c2.getB() + "i");
-		System.out.println("The sum of two comlex numbers c1 + c2 = " + z.getA() + " + " + z.getB() + "i");
+		System.out.println("The sum of two complex numbers c1 + c2 = " + z.getA() + " + " + z.getB() + "i");
 		
 		z = z.diff(c1, c2);
-		System.out.println("The difference of two comlex numbers c1 - c2 = " + z.getA() + " + " + z.getB() + "i");
+		System.out.println("The difference of two complex numbers c1 - c2 = " + z.getA() + " + " + z.getB() + "i");
+		
+		z = z.div(c1, c2);
+		System.out.println("The result of division complexs numbers c1/c2 = " + z.getA() + " + " + z.getB() + "i");
+		
 		
 	}
 
--- src/ComplexNumber.java
+++ src/ComplexNumber.java
@@ -1,53 +1,81 @@
 
 public class ComplexNumber {
-	private double a; //real part
-	private double b; //imaginary part
-	
-	/** The constructor generates a complex number (a,b) with parameters
-	 * @param a number of a double type representing the real part of the complex number
-	 * @param b number of a double type representing the imaginary part of the complex number
+	private double a; // real part
+	private double b; // imaginary part
+
+	/**
+	 * The constructor generates a complex number (a,b) with parameters
+	 * 
+	 * @param a number of a double type representing the real part of the complex
+	 *          number
+	 * @param b number of a double type representing the imaginary part of the
+	 *          complex number
 	 */
 	public ComplexNumber(double a, double b) {
 		this.a = a;
 		this.b = b;
 	}
+
 	/**
 	 * @return the a, the real part of the complex number
 	 */
 	public double getA() {
 		return a;
 	}
+
 	/**
 	 * @return the b, the imaginary part of the complex number
 	 */
 	public double getB() {
 		return b;
 	}
+
 	/**
-	 * The method returns the sum of two complex numbers x and y 
+	 * The method returns the sum of two complex numbers x and y
+	 * 
 	 * @param x
 	 * @param y
 	 * @return
 	 */
 	public ComplexNumber add(ComplexNumber x, ComplexNumber y) {
-		ComplexNumber z = new ComplexNumber(0,0);
+		ComplexNumber z = new ComplexNumber(0, 0);
 		z.a = x.a + y.a;
 		z.b = x.b + y.b;
 		return z;
 	}
+
 	/**
-	 * The method returns the sum of two complex numbers x and y 
+	 * The method returns the sum of two complex numbers x and y
+	 * 
 	 * @param x
 	 * @param y
 	 * @return
 	 */
 	public ComplexNumber diff(ComplexNumber x, ComplexNumber y) {
-		ComplexNumber z = new ComplexNumber(0,0);
+		ComplexNumber z = new ComplexNumber(0, 0);
 		z.a = x.a - y.a;
 		z.b = x.b - y.b;
 		return z;
 	}
-	
-	
 
+	/**
+	 * The method returns the result of the division of a complex number x by y
+	 * 
+	 * @param x numerator
+	 * @param y denominator
+	 * @return
+	 */
+	public ComplexNumber div(ComplexNumber x, ComplexNumber y) {
+
+		ComplexNumber z = new ComplexNumber(1,1);
+		try {
+			double den = y.a * y.a + y.b * y.b;
+			z.a = (x.a * y.a + x.b * y.b) / den;
+			z.b = (x.b * y.a - x.a * y.b) / den;
+			return z;
+		} catch (ArithmeticException e) {
+			System.out.println("The division by zero impossible ");
+		}
+		return z;
+	}
 }
